4233e76d733ecfbcdaa53ab0b49a052c1b89d5be
gotomark <dmitriy.yakynin@gmail.com>
2022-07-05 12:48:04 +0300

    max time curl

---
+++ app/Console/Commands/ClearAllData.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TmpPhotoGroup;
+use Illuminate\Console\Command;
+
+class ClearAllData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'resetData:tmp_and_files';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $tmp = TmpPhotoGroup::all();
+
+        foreach ($tmp as $item) {
+            if($item->file_path != '') {
+                $this->removeFolder(storage_path() . '/app/public/' . $item->media_group_id);
+            }else{
+                unlink(storage_path() . '/app/public/' .$item->media_group_id);
+            }
+            die();
+        }
+
+        $tmp->truncate();
+
+    }
+
+    function removeFolder($folderName) {
+        if (is_dir($folderName))
+            $folderHandle = opendir($folderName);
+
+        if (!$folderHandle)
+            return false;
+
+        while($file = readdir($folderHandle)) {
+            if ($file != "." && $file != "..") {
+                if (!is_dir($folderName."/".$file))
+                    unlink($folderName."/".$file);
+                else
+                    removeFolder($folderName.'/'.$file);
+            }
+        }
+
+        closedir($folderHandle);
+        rmdir($folderName);
+        return true;
+    }
+}
